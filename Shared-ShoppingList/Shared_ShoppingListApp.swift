bf77c4e44cbf9e26f7e16232b4b8b73b9ec466a7
Greg Hughes <dcgg_101@hotmail.com>
2021-01-21 19:00:17 -0700

    Initial Commit

---
+++ Shared-ShoppingList/Shared_ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shared_ShoppingListApp.swift
+//  Shared-ShoppingList
+//
+//  Created by Greg Hughes on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Shared_ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
